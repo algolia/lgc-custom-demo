e062c80a44d06ea52c0c2f2f37c142cfece497a5
Alexandre Viretti <alexou73@gmail.com>
2021-09-09 10:50:22 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <ais-instant-search
+      :search-client="searchClient"
+      index-name="rayban_merged"
+    >
+      <Header @showFederatedSearch="showFederatedSearchMethod" />
+      <HomePage v-if="!getCatOne && !getCatTwo" />
+      <CatOne v-if="getCatOne" />
+      <CatTwo v-if="getCatTwo" />
+      <Modal v-if="getShowModal" />
+    </ais-instant-search>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import HomePage from "@/components/HomePage";
+import Modal from "@/components/Modal";
+import CatOne from "@/components/CatOne";
+import CatTwo from "@/components/CatTwo";
+import { mapGetters } from "vuex";
+import algoliasearch from "algoliasearch/lite";
+
+export default {
+  components: {
+    Header,
+    HomePage,
+    Modal,
+    CatOne,
+    CatTwo,
+  },
+  data() {
+    return {
+      showFederatedSearch: true,
+      searchClient: algoliasearch(
+        "JDBD6EJM33",
+        "0fe54b2e3991d370c91376981aff9d48"
+      ),
+    };
+  },
+  methods: {
+    showFederatedSearchMethod(value) {
+      this.showFederatedSearch = value;
+    },
+  },
+  computed: {
+    ...mapGetters("HeaderModule", [
+      "getShowFederatedSearch",
+      "getShowModal",
+      "getCatTwo",
+      "getCatOne",
+    ]),
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/CarouselHome/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="carousel-container">
+    <h3>Our Iconic Aviator</h3>
+    <ais-configure :hits-per-page.camel="7" ruleContexts="aviator"/>
+    <HitsCarouselHome />
+  </div>
+</template>
+
+<script>
+import HitsCarouselHome from "@/components/HitsCarouselHome";
+export default {
+  components: {
+    HitsCarouselHome,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.carousel-container {
+  width: 90%;
+  margin: 5rem auto;
+  h3 {
+    font-family: "Oswald", sans-serif;
+    color: #000;
+    font-size: 2rem;
+  }
+}
+</style>
