2a1555542a71531abdb8805713ed0df2c7a51d5a
Alexandre Viretti <alexou73@gmail.com>
2021-09-14 17:08:09 +0200

    fix(carouselHome): add new instant search for the carousel

--- src/components/CarouselHome/index.vue
+++ src/components/CarouselHome/index.vue
@@ -1,14 +1,28 @@
 <template>
-  <div class="carousel-container">
-    <h3>Our Products</h3>
-    <ais-configure :hits-per-page.camel="7"/>
-    <HitsCarouselHome />
-  </div>
+  <ais-instant-search
+    :search-client="searchClient"
+    :index-name="appConfig.mainIndex"
+  >
+    <div class="carousel-container">
+      <h3>Our Products</h3>
+      <ais-configure :hits-per-page.camel="7" />
+      <HitsCarouselHome />
+    </div>
+  </ais-instant-search>
 </template>
 
 <script>
 import HitsCarouselHome from "@/components/HitsCarouselHome";
+import algoliasearch from "algoliasearch/lite";
+import { appConfig } from "../../configuration";
 export default {
+  data() {
+    return {
+      appConfig,
+      showFederatedSearch: true,
+      searchClient: algoliasearch(appConfig.appId, appConfig.apiKey),
+    };
+  },
   components: {
     HitsCarouselHome,
   },
