f8a7caa130d4ad2d8fbbce895c25fc86089407c2
Hugo Valla <hugo_valla@hotmail.fr>
2021-09-14 19:35:38 +0200

    pdp

--- src/components/ProductDetails/index.vue
+++ src/components/ProductDetails/index.vue
@@ -2,40 +2,67 @@
   <div>
     <p class="close-modal" @click="closeModal">X</p>
     <div class="modal-detail">
+          <h2>{{ getProductDetails.name }}</h2>
       <div class="product-side">
+        <div class="modal-infos">
+          <p>{{ getProductDetails.description }}</p>
+           <div class="infos">
+               <div>
+                <p>Product code : <span class="bold">{{getProductDetails.code}}</span></p>
+                <p>Molecular formula: <span class="bold">{{getProductDetails.listMolecularFormula[0]}}</span> </p>
+                <p>Cas number: <span class="bold">{{getProductDetails.listCASNumber[0]}}</span></p>
+                </div>
+                <div>
+                <p>Molecular weight: <span class="bold">{{getProductDetails.listMolecularWeight[0]}}</span></p>
+                <p>Matrix: <span class="bold">{{getProductDetails.variantMatrix}}</span></p>
+                <p>ProductType: <span class="bold">{{getProductDetails.productType}}</span></p>
+                </div>
+            </div>
+       
+        </div>
         <div class="modal-images">
           <img
             :src="'https://www.lgcstandards.com/' + getProductDetails.brand.logo.url"
             alt=""
           />
         </div>
-      </div>
-      <div class="fake-filters">
-        <div class="modal-infos">
-          <h3>{{ getProductDetails.name }}</h3>
-          <p>${{ getProductDetails.price }}</p>
-          <p>{{ getProductDetails.description }}</p>
+    </div>
+    <div class="stock-infos">
+      <p class="pack-size">Pack size : </p>
+      <div class="details">
+        <a class="stock">Stock: {{getProductDetails.stockQty}}</a>
+        <div>
+        <p>Estimated dispatch</p>
+        <p>{{getProductDetails.stockmessage}}</p>
         </div>
-        <img :src="pdp" alt="" />
+      </div>
+      <div>
+        <img :src="addToCart" alt="">
       </div>
     </div>
-    <CarouselProducts />
+    </div>
+    <div class="modal-image">
+      <img :src="pdp" alt="">
+    </div>
+    <!-- <CarouselProducts /> -->
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
 import pdp from "@/assets/images/pdp.png";
+import addToCart from "@/assets/images/addToCart.png";
 
-import CarouselProducts from "@/components/CarouselProducts";
+// import CarouselProducts from "@/components/CarouselProducts";
 
 export default {
   components: {
-    CarouselProducts,
+    // CarouselProducts,
   },
   data() {
     return {
       pdp,
+      addToCart
     };
   },
   computed: {
@@ -48,25 +75,87 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "@/assets/scss/variables/variables.scss";
 .modal-detail {
-  background-color: transparent;
-  display: -webkit-flex;
-  display: flex;
   padding: 2rem;
   width: 100%;
   border-radius: 3px;
+  background-color: #e9ebee;
   .product-side {
-    margin: 0 auto;
-    width: 80%;
-    .modal-images {
+    margin: 2rem auto;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .modal-infos{
+      width: 80%;
+    }
+
+    .infos{
       display: flex;
-      align-items: center;
-      width: 100%;
-      img {
-        width: 90%;
+      justify-content: space-evenly;
+      width: 90%;
+      background-color: white;
+      border-radius: 10px;
+      div{
+        margin: 1rem;
+
+        p{
+          margin-top: 1rem;
+          font-weight: 300;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          
+        }
+
+        .bold{font-weight: 500;}
       }
     }
+
+    .modal-images{
+      background-color: white;
+      border-radius: 10px;
+      height: 100%;
+      width: 30%;
+      height: 11.5rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      img{width: 70%;}
+    }
   }
+  
+
+  .stock-infos{
+    background-color: white;
+    border-radius: 10px;
+    padding: 1rem;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+
+    .pack-size{
+      width: 20%;
+    }
+    .details{
+      width: 40%;
+      div{
+
+        margin-top: 1rem;
+      }
+      }
+    .stock{
+      font-size: 1.2rem;
+      font-weight: bold;
+      font-family: $main-font;
+      margin-bottom: 2rem;
+    }
+
+
+  }
+
   .fake-filters {
     width: 60%;
     .modal-infos {
@@ -110,6 +199,17 @@ export default {
     }
   }
 }
+
+
+    .modal-image {
+      display: flex;
+      align-items: center;
+      width: 100%;
+     
+      img {
+        width: 100%;
+      }
+    }
 .close-modal {
   position: absolute;
   right: 2rem;
